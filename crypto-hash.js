30dc76bb9914d322b5fadec77c4a1528fdea850f
Richard Anabeto Opoku <anabeto93@gmail.com>
2020-07-24 18:18:56 +0000

    corrected the hash algorith, during sorting.

--- crypto-hash.js
+++ crypto-hash.js
@@ -4,22 +4,23 @@ const cryptoHash = (...args) => {
     const hash = crypto.createHash('sha256')
 
     let temp = "";
-    
+
     if (args.length === 1) {
-        if (args !== null && typeof(args) === 'string') {
-            temp = JSON.stringify(args[0]).replace (/(^")|("$)/g, '');
-        } else if(args !== null && typeof(args) === 'object') {
+        if (args !== null && typeof(args[0]) === 'string') {
+            temp = args[0]
+        } else if(args !== null && typeof(args[0]) === 'object') {
             temp = JSON.stringify(args[0]).replace (/(^")|("$)/g, '')
         }
     } else {
         temp = args.sort().map(e => {
             if (e !== null && typeof(e) === 'object') {
-                return JSON.stringify(e).replace (/(^")|("$)/g, '')
+                return JSON.stringify(e)
             }
+
+            return e
         }).join(' ');
     }
     
-
     hash.update(temp)
 
     return hash.digest('hex')
