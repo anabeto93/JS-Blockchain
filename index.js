2495bd6fdfc0b64e34263a43ef00c52bd4cb2e7b
Richard Anabeto Opoku <anabeto93@gmail.com>
2020-07-25 12:06:16 +0000

    blockchain instances can now broadcast chains to one another.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const Blockchain = require('./blockchain')
+const PubSub = require('./pubsub')
 
 const app = express()
 app.use(bodyParser.json())
 const blockchain = new Blockchain()
+const pubsub = new PubSub({ blockchain })
+
+pubsub.broadcastChain()
 
 
 app.get('/api/blocks', (req, res) => {
@@ -19,8 +23,15 @@ app.post('/api/mine', (req, res) => {
     res.json({ message: "Block mined", error_code: 201 })
 })
 
+const DEFAULT_PORT = 3000
+let PEER_PORT
 
-const PORT = 3000
+if (process.env.GENERATE_PEER_PORT === 'true') {
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000)
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT
+
 app.listen(PORT, () => {
     console.log(`Listening on port localhost:${PORT}`)
 })
